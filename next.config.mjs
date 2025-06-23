ad773d00e5dcf183a969adcb16a7854204e3ee19
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-23 10:27:05 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [], // Adicione domínios externos se usar imagens remotas
+  },
+};
 
+export default nextConfig;
