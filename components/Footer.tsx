67363856539b5d83901e4a216bfb1e619a006872
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:19:00 -0300

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center text-sm text-gray-500 py-6 mt-12 border-t">
-      © {new Date().getFullYear()} Libertabra. Todos os direitos reservados.
-    </footer>
-  );
-}
