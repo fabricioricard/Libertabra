04888608aac9ad193608097fd3a771a2316e3632
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:12:27 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className="pt-16 bg-white text-gray-900">
+      <body className="pt-16 bg-gradient-to-b from-white to-blue-50 min-h-screen text-gray-800">
         <Header />
-        <main className="min-h-screen max-w-6xl mx-auto px-4">{children}</main>
+        <main className="max-w-6xl mx-auto px-4">{children}</main>
         <Footer />
       </body>
     </html>
