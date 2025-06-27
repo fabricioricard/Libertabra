45825293f6c18c0291216ca0fe4a444ed655cdad
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:22:41 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [], // Adicione domínios externos se usar imagens remotas
-  },
-};
-
-export default nextConfig;
