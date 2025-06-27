841a80881c381592b94418e061caea0d3936e8a6
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:05:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ReactNode } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Libertabra - Construindo a Liberdade",
-  description: "Comunidade libertária para uma cidade privada autossustentável",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
+      <body className="pt-16 bg-white text-gray-900">
         <Header />
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen max-w-6xl mx-auto px-4">{children}</main>
         <Footer />
       </body>
     </html>
